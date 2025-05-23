858cc8ed2892ad07042d5dbe070a7d8a4bed6d75
Conner Hoelzle <connerhoelzle@gmail.com>
2025-05-23 12:04:52 -0500

    inserted first two receipts worth of data

---
+++ insert_2025-05-22_purchases.sql
@@ -0,0 +1,20 @@
+INSERT INTO Stores(Name, Location) 
+VALUES
+    ("Walmart", "Dallas Neighborhood Market"),
+    ("Target", "Cityplace Market")
+;
+
+INSERT INTO Receipts(StoreID, TripDateTime)
+VALUES
+    (1, '2025-05-22T20:17:18'),
+    (2, '2025-0522T16:50:00')
+;
+
+INSERT INTO Items(ReceiptID, Name, Quantity, OriginalUnitPrice, PricePaid, Notes)
+VALUES
+    (1, '6oz Shredded Parmesan Cheese', 1, 2.24, 2.24, 'Great Value'),
+    (2, 'Jeans', 2, 20.00, 20.00, 'Goodfellow & Co.'),
+    (2, 'Baking Soda', 1, 0.99, 0.99, 'Good & Gather'),
+    (2, 'Eucerin', 1, 12.99, 12.99, 'Eczema Cream'),
+    (2, 'Toilet Paper', 1, 7.99, 7.99, 'Charmin Strong')
+;
