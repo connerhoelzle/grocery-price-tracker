41fe6c83a9a9a522f36a46dfc3b37785beffa523
Conner Hoelzle <connerhoelzle@gmail.com>
2025-05-23 10:40:58 -0500

    Initial commit: database schema

--- schema.sql
+++ schema.sql
@@ -1,31 +1,40 @@
--- Create the stores table
-
 CREATE TABLE Stores (
-	ID			INTEGER		PRIMARY KEY,
-	Name			TEXT		NOT NULL,
-	LOCATION		TEXT		NOT NULL
-);
-
--- Create the receipts table
+    ID                  INTEGER PRIMARY KEY,
+    Name                TEXT    NOT NULL,
+    Location            TEXT    NOT NULL
+)
+;
 
 CREATE TABLE Receipts (
-	ID			INTEGER		PRIMARY KEY,
-	StoreID			INTEGER		NOT NULL,
-	TripDateTime		TEXT		NOT NULL, -- use ISO 8601
-	Notes			TEXT,	-- optional
-	FOREIGN KEY (StoreID) REFERENCES Stores(ID)
-);
-
--- grCreate the Items table
+    ID                  INTEGER PRIMARY KEY,
+    StoreID             INTEGER NOT NULL,
+    TripDateTime        TEXT    NOT NULL -- ISO 8601: 'YYYY-MM-DDTHH:MM:SS',
+    Notes               TEXT,
+    FOREIGN KEY (StoreID) REFERENCES Stores(ID)
+)
+;
 
 CREATE TABLE Items (
-	ID			INTEGER		PRIMARY KEY,
-	ReceiptID		INTEGER		NOT NULL,
-	Name			TEXT		NOT NULL,
-	OriginalPrice		REAL		NOT NULL,
-	DiscountedPrice		REAL		NOT NULL,
-	TruePrice		REAL, -- optional, can be calculated during analysis
-	Category		TEXT, -- optional
-	Notes			TEXT, -- optional
-	FOREIGN KEY (ReceiptID) REFERENCES Receipts(ID)
-);		     
+    ID                  INTEGER PRIMARY KEY,
+    ReceiptID           INTEGER NOT NULL,  
+    Name                TEXT    NOT NULL,
+    Quantity            REAL    NOT NULL,
+    OriginalUnitPrice   REAL    NOT NULL,
+    DiscountedUnitPrice REAL    NOT NULL,
+    PricePaid           REAL    NOT NULL,
+    Notes               TEXT,
+    FOREIGN KEY (ReceiptID) REFERENCES Receipts(ID)
+)
+;
+
+CREATE TABLE ReferencePrices (
+    ID                  INTEGER PRIMARY KEY,
+    ItemName            TEXT    NOT NULL,
+    StoreID             INTEGER NOT NULL,
+    UnitPrice           REAL    NOT NULL,
+    Date                TEXT    NOT NULL, -- ISO 8601 'YYYY-MM-DD'
+    Notes               TEXT,
+    FOREIGN KEY (ItemName) REFERENCES Items(Name),
+    FOREIGN KEY (StoreID)  REFERENCES Stores(ID) 
+)
+;
