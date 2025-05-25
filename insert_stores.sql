54439f799bd72f7a1c597f66904ff6948b03ee0b
Conner Hoelzle <connerhoelzle@gmail.com>
2025-05-24 21:08:43 -0500

    Added a few local stores

---
+++ insert_stores.sql
@@ -0,0 +1,7 @@
+insert into stores(name, location)
+values 
+    ('ALDI', 'DEN 42'),
+    ('Kroger', 'Capitol Ave'),
+    ('Whole Foods Market', 'Uptown Dallas'),
+    ("Trader Joe's", 'Dallas Knox')
+;
