438c3b298415340065724ce5ecd6ca7e7c36c870
Conner Hoelzle <connerhoelzle@gmail.com>
2025-05-27 13:31:57 -0500

    updated insert_data.sql to reflect new schema

--- insert_data.sql
+++ insert_data.sql
@@ -1,4 +1,3 @@
 .read insert_stores_data.sql
-.read insert_receipts_data.sql
-.read insert_items_data.sql
-.read insert_reference-prices_data.sql
+.read insert_products_data.sql
+.read insert_productprices_data.sql
