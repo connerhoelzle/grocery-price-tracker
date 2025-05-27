48e1dc5901168141fa2657564ec0a5082072bf83
Conner Hoelzle <connerhoelzle@gmail.com>
2025-05-27 13:39:04 -0500

    removing deleted files

--- insert_2025_05_24_prices.sql
+++
@@ -1,9 +0,0 @@
-insert into ReferencePrices(ItemName, StoreID, UnitPrice, Date)
-values
-    ('Banana', 1, 0.28, '2025-05-24'),
-    ('Banana', 2, 0.29, '2025-05-24'),
-    ('Banana', 3, 0.17, '2025-05-24'),
-    ('Banana', 4, 0.23, '2025-05-24'),
-    ('Banana', 5, 0.55, '2025-05-24'),
-    ('Banana', 6, 0.23, '2025-05-24')
-;
--- insert_stores.sql
+++
@@ -1,9 +0,0 @@
-insert into stores(name, location)
-values 
-    ('Walmart', 'Dallas Neighborhood Market'),
-    ('Target', 'Cityplace Target'),
-    ('ALDI', 'DEN 42'),
-    ('Kroger', 'Capitol Ave'),
-    ('Whole Foods Market', 'Uptown Dallas'),
-    ("Trader Joe's", 'Dallas Knox')
-;
